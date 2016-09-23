16799392e020a610bf782a20ec9cb73fd459c17c
Alice Darner <alice.darner@gmail.com>
2016-09-23 11:18:43 +0200

    fixed some misspellings

--- GameOfLife/src/gameoflife/GameOfLife.java
+++ GameOfLife/src/gameoflife/GameOfLife.java
@@ -1,6 +1,6 @@
 /* This is a Newton YH Project for Application  2016.
- * This is not propriatary in any way, but please spread the word if t is in any way useful for you.
- * We are a group of four people looking for a job :D
+ * This is not propriatary in any way, but please spread the word if it is in any way useful for you.
+ * We are a group of four people looking for a job each :D
  */
 package gameoflife;
 
