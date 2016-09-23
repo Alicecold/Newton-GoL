4752751f1e1f423a97c9264a299e17aaa93051ab
JacobAhlberg <jacahlberg@hotmail.com>
2016-09-23 10:57:26 +0200

    testing
    Skrivut nej

--- GameOfLife/src/gameoflife/GameOfLife.java
+++ GameOfLife/src/gameoflife/GameOfLife.java
@@ -15,7 +15,7 @@ public class GameOfLife {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Nej");
     }
     
 }
