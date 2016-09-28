5ed8f11a8253f50877978b70cf3b7794a12d5c91
Alice Darner <alice.darner@gmail.com>
2016-09-28 17:39:38 +0200

    GUI is now reachable by import package
    I thought that might be helpful :) //Waffle of the impending doom :boom:
    :strawberry:

--- GameOfLife/src/gameoflife/GameOfLife.java
+++ GameOfLife/src/gameoflife/GameOfLife.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package gameoflife;
+
+import gui.*; //This is how you do it! /Alice
 /**
  *
  * @author alicecold
@@ -14,9 +16,10 @@ public class GameOfLife {
      * @param args the command line arguments
      */
     public static void main(String[] args)  {
-        gridUI ui = new gridUI();
+        //gridUI ui = new gridUI();
         
-        ui.show();
+        GUIGrid ui = new GUIGrid();// This is how you do it! / Alice ('w')
+        ui.setVisible(true); //I copied Jacobs file to the correct folder, so that everything that Jacob made is correctly useable :)
         
     }
     
