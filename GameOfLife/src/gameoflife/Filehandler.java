f6068d10b45f92d40bf12c9e65f0a77381848334
Alice Darner <alicecoldangel@gmail.com>
2016-10-08 16:46:40 +0200

    updated a comment ;)

--- GameOfLife/src/gameoflife/Filehandler.java
+++ GameOfLife/src/gameoflife/Filehandler.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* This is a Newton YH Project for Application  2016.
+ * This is not propriatary in any way, but please spread the word if it is in any way useful for you.
+ * We are a group of four people looking for a job each :D
  */
 package gameoflife;
 
